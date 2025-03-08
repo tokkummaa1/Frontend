3dcccc845203d119d8d65cc50a449aad827bb078
joniyad <j649618@gmail.com>
2025-03-08 23:59:16 +0300

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 import LogInView from "@/views/LogInView.vue";
 import SignUpView from "@/views/SignUpView.vue";
 import ResetPasswordView from "@/views/ResetPasswordView.vue";
